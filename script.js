b636b2282f347c6cd20124c11f3085d3c52a5395
daiki-wakabayashi <wakabayashidaiki0808@gmail.com>
2025-01-15 17:55:46 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+// CSVデータを処理する関数
+function processData(csvData) {
+    // CSVデータを行に分割
+    const rows = csvData.split("\r\n");
+    // 各行をカンマで分割し、2次元配列に変換
+    const data = rows.map(row => row.split(","));    
+    // データを返す
+    return data;
+}
+
+// 読み込んだCSVデータをグラフ用に処理する関数
+function processGraphData(graphData) {
+    // データをforループさせて新しい配列に転置する
+    for (var i = 1 ; i < graphData.length - 1 ; i++) {
+    timedata.push(graphData[i][0]);
+    swadata.push(graphData[i][1]);
+    swtdata.push(graphData[i][2]);
+    yrdata.push(graphData[i][3]);
+    xdata.push(graphData[i][4]);
+    ydata.push(graphData[i][5]);
+    }
+}
+
+
+// グラフを描画するための関数
+function processGraph(canId,labelname,labeldata,graphdata) {
+    var ctx = document.getElementById(canId);  
+    var myGraph = new Chart(ctx,{
+    type: 'line',
+    data: {
+        labels: labeldata,
+        datasets: [{
+        label: labelname,
+        data: graphdata.map(Number),
+        borderColor: 'rgba(60, 190, 20, 0.8)'
+        }],
+    },
+    }); 
+}
