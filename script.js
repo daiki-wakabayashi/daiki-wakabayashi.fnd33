4527274c614c4de186b9243ef15ccb51d12a45ec
daiki-wakabayashi <wakabayashidaiki0808@gmail.com>
2025-01-17 13:27:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,31 +12,57 @@ function processData(csvData) {
 }
 
 // 読み込んだCSVデータをグラフ用に処理する関数
-function processGraphData(graphData) {
+function processGraphDataCreate(graphData) {
     // データをforループさせて新しい配列に転置する
     for (var i = 1 ; i < graphData.length - 1 ; i++) {
     timedata.push(graphData[i][0]);
     swadata.push(graphData[i][1]);
     swtdata.push(graphData[i][2]);
     yrdata.push(graphData[i][3]);
-    xdata.push(graphData[i][4]);
-    ydata.push(graphData[i][5]);
+    xdata.push(graphData[i][5]);
+    ydata.push(graphData[i][6]);
     }
 }
 
-
 // グラフを描画するための関数
-function processGraph(canId,labelname,labeldata,graphdata) {
+function processGraph(canId,labelname,labeldata,graphdata,xlabel,ylabel) {
     var ctx = document.getElementById(canId);  
+
+    const plotObject = processObjectCreate(labelname,labeldata,graphdata);
+
     var myGraph = new Chart(ctx,{
     type: 'line',
-    data: {
+    data: plotObject,
+
+    options: {
+        scales: {
+            x:{
+                display: true,
+                title:{
+                    display: true,
+                    text: xlabel
+                }
+            },  
+            y:{
+                display: true,
+                title:{
+                    display: true,
+                    text: ylabel
+                },      
+            },
+        }
+    }
+    }); 
+}
+
+function processObjectCreate(labelname,labeldata,graphdata) {
+    const result = {
         labels: labeldata,
         datasets: [{
         label: labelname,
         data: graphdata.map(Number),
         borderColor: 'rgba(60, 190, 20, 0.8)'
         }],
-    },
-    }); 
+    };
+    return result;
 }
